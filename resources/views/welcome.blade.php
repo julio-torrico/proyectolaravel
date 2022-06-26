8bb7bd723a270f11dc150ea11488bca14541ad29
julio-torrez <julioces_torrez@gmail.com>
2022-06-26 13:13:22 -0400

    ya esta mi ultima modificacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -118,7 +118,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                                Provando esta pagina
                             </a>
                         </div>
                     </div>
